6177299bf50b5b7362f153131cfd46a22e6a55c1
Tej Kashi <pct960@gmail.com>
2025-07-21 12:44:48 -0400

    WIP: Cleaning up logging across modules

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -12,11 +12,11 @@
 package main
 
 import (
-	"log"
 	"os"
 	"path/filepath"
 
 	"github.com/pgedge/ace/internal/cli"
+	"github.com/pgedge/ace/internal/logger"
 	"github.com/pgedge/ace/pkg/config"
 )
 
@@ -25,18 +25,18 @@ func main() {
 	if _, err := os.Stat(cfgPath); os.IsNotExist(err) {
 		execPath, err := os.Executable()
 		if err != nil {
-			log.Fatalf("unable to determine executable path: %v", err)
+			logger.Fatal("unable to determine executable path: %v", err)
 		}
 		root := filepath.Dir(filepath.Dir(execPath))
 		cfgPath = filepath.Join(root, "ace.yaml")
 	}
 	if err := config.Init(cfgPath); err != nil {
-		log.Fatalf("loading config (%s): %v", cfgPath, err)
+		logger.Fatal("loading config (%s): %v", cfgPath, err)
 	}
 
 	app := cli.SetupCLI()
 	err := app.Run(os.Args)
 	if err != nil {
-		log.Fatalf("Error: %v", err)
+		logger.Error("%v", err)
 	}
 }
