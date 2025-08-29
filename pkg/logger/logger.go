2efeeb14a3e3d5cf86d6fc681e8b750f6fbc0105
Tej Kashi <pct960@gmail.com>
2025-08-29 14:07:06 -0400

    Add licence header to source and config files

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -1,3 +1,14 @@
+// ///////////////////////////////////////////////////////////////////////////
+//
+// # ACE - Active Consistency Engine
+//
+// Copyright (C) 2023 - 2025, pgEdge (https://www.pgedge.com/)
+//
+// This software is released under the PostgreSQL License:
+// https://opensource.org/license/postgresql
+//
+// ///////////////////////////////////////////////////////////////////////////
+
 package logger
 
 import (
